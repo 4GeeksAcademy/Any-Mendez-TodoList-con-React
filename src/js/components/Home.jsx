a3435e0a33fbd3315c6e1ff655c9132236d642be
anymen331 <anymen331@gmail.com>
2025-03-06 01:00:39 +0000

    se terminaron cambios en Home.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,11 +6,15 @@ const Home = () => {
   const [inputValue] = useState("");
   const [tareas, setTareas] = useState([]);
   const [nuevaTarea, setNuevaTarea] = useState("");
+  const [iconoVisible, setIconoVisible] = useState(false);
+  const [indiceTareaSeleccionada, setIndiceTareaSeleccionada] = useState(null);
+
   const agregarTarea = (e) => {
     if (e.key === "Enter" && nuevaTarea.trim() !== "") {
       setTareas([...tareas, nuevaTarea]); setNuevaTarea("");
     }
   };
+
   const borrarTarea = (index) => {
     setTareas(tareas.filter((t, i) => i !== index));
   };
@@ -18,29 +22,44 @@ const Home = () => {
   return (
 
     <div className="container">
-      <div className="card border-solid-1 mt-5 bg-secondary border-radius-3 text-white">
+      <div className="card mt-5" style={{backgroundColor: 'rgb(251, 251, 242)'}}>
 
         <h1 className="title text-center">
           Lista de Tareas
         </h1>
-        <input className="form-control-lg" size={40} placeholder="¿Qué necesitas hacer?" value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)} onKeyDown={agregarTarea}>
+        <div className="row col-md-8 mx-auto text-break mb-5" style={{border: '1px solid #ccc'}}>
+        <input className="card border-none " placeholder="¿Qué necesitas hacer?" value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)} onKeyDown={agregarTarea}>
         </input>
-        <ul className="list-group list-group-flush mt-2">
+        <ul className="list-group list-group-flush text-muted px-0">
           {tareas.length === 0 ? (
-            <li className="list-group-item text-muted text-center">
+            <li className="list-group-item text-muted text-center mt-3" style={{backgroundColor: 'rgb(251, 251, 242)'}}>
               No hay tareas, añadir tareas
-            </li>) : tareas.map((item, index) => {
+            </li>) : (tareas.map((item, index) => {
 
-              return <li key={index} className="d-flex justify-content-between">
-                {item} <i className="far fa-trash-alt mx-5" onClick={() =>
-                  setTareas(tareas.filter((t, currentIndex) => index != currentIndex))}></i>
+              return (<li key={index} className="d-flex justify-content-between mt-3" style={{borderBottom: '1px solid #ccc'}} onMouseOver={() => {
+              setIconoVisible(true);
+              setIndiceTareaSeleccionada(index);
+            }}
+            onMouseOut={() => {
+              setIconoVisible(false);
+              setIndiceTareaSeleccionada(null);
+            }}
+          >
+            {item}
+            {iconoVisible && indiceTareaSeleccionada === index && (
+                <i className="far fa-trash-alt mx-5" onClick={() =>
+                  setTareas(tareas.filter((t, currentIndex) => index !== currentIndex))}></i>
+                )
+              }
               </li>
-            })}
+              );
+            }))}
+             <div className="text text-muted mt-3 mx-3">{tareas.length} tareas</div>
         </ul>
-        <div className="card bg-secondary">{tareas.length}</div>
+      </div>
       </div>
     </div>
   );
 };
 
-export default Home
+export default Home;
